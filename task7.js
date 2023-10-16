4c433a2274199df6b72b43b6db0af37a2e6d424c
Дмитрий <dim23062006@gmail.com>
2023-10-16 13:53:10 +0300

    task7

---
+++ task7.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+const sentence = readlineSync.question('Введите предложение: ');
+function countWords(sentence) {
+    let count = sentence.split(' ').length;
+    console.log(count);
+
+}
+
+countWords(sentence);
