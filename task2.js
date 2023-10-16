396966fe4376f6b464126935e10a4bb949e3d2d8
Дмитрий <dim23062006@gmail.com>
2023-10-16 13:04:08 +0300

    task2

--- task2.js
+++ task2.js
@@ -4,6 +4,6 @@ function celsiusToFahrenheit (celsius) {
     const convertation = (celsius * 9 / 5) + 32;
     console.log(convertation);
     return convertation;
-}
+};
 
 celsiusToFahrenheit(celsius);
