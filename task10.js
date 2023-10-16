4597e02b469c3c51912b1256cf29474d99df6087
Дмитрий <dim23062006@gmail.com>
2023-10-16 14:19:39 +0300

    task10

---
+++ task10.js
@@ -0,0 +1,11 @@
+function generateRandomEquation() {
+    const a = (Math.random() * 100).toFixed(0);
+    const b = (Math.random() * 100).toFixed(0);
+    const x = (Math.random() * 100).toFixed(0);
+    const c = (Math.random() * 100).toFixed(0);
+
+    const formule = (a * x + b) === c;
+    console.log(a + ' \* ' + x + ' + ' + b + ' = ' + c + ' -> ' + formule);
+}
+
+generateRandomEquation();
