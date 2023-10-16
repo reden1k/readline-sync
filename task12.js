b46beb66098b8299a36ab5f50c9fc681c990a802
reden1k <dim23062006@gmail.com>
2023-10-16 21:07:13 +0300

    task12

---
+++ task12.js
@@ -0,0 +1,22 @@
+const readlineSync = require('readline-sync');
+const str = readlineSync.question('Введите текст: ');
+const exception = readlineSync.question('Введите слово исключение: ');
+function reverseStringWithException(str, exception) {
+    let massive = str.split(' ');
+    let reversedText = '';
+    for (let i = 0; i <= massive.length - 1;) {
+        let word = massive[i];
+        i += 1;
+        let lowerCaseWord = word.toLowerCase();
+        if (lowerCaseWord === exception) {
+            reversedText += word + ' ';
+            continue;
+        }
+        reversedText += word.split('').reverse().join('') + ' ';
+        
+    }
+    console.log(reversedText);
+    return reversedText;
+};
+
+reverseStringWithException(str, exception);
