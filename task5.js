f1860418ce5c9ef4d933e8d48e4f918b6cba1562
Дмитрий <dim23062006@gmail.com>
2023-10-16 13:34:41 +0300

    task5

---
+++ task5.js
@@ -0,0 +1,32 @@
+const readlineSync = require('readline-sync');
+const x1 = parseInt(readlineSync.question('Введите x1: '));
+const y1 = parseInt(readlineSync.question('Введите y1: '));
+const x2 = parseInt(readlineSync.question('Введите x2 '));
+const y2 = parseInt(readlineSync.question('Введите y2: '));
+
+function calculateDistance(x1, y1, x2, y2) {
+    let resultX1 = 0;
+    let resultX2 = 0;
+    if (x1 > y1) {
+        resultX1 = x1 - y1;
+    }
+    if (y1 > x1) {
+        resultX1 = y1 = x1;
+    }
+    if (x2 > y2) {
+        resultX2 = x2 - y2;
+    }
+    if (y2 > x2) {
+        resultX2 = y2 - x2;
+    }
+    if (x1 === y1) {
+        resultX1 = 0;
+    }
+    if (x2 === y2) {
+        resultX2 = 0;
+    }
+    console.log(resultX1 + ' ' + resultX2);
+    return resultX1, resultX2;
+}
+
+calculateDistance(x1, y1, x2, y2);
