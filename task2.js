bac407b46529b3fafaa082a3de7591c0a57319c2
Дмитрий <dim23062006@gmail.com>
2023-10-16 12:11:36 +0300

    task2

---
+++ task2.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+const celsius = readlineSync.question('Введите градусы в Цельсиях: ');
+function celsiusToFahrenheit (celsius) {
+    const convertation = (celsius * 9 / 5) + 32;
+    console.log(convertation);
+    return convertation;
+}
+
+celsiusToFahrenheit(celsius);
